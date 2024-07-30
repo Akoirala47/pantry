0834bf2165fbb4376450064a411615d197f5119c
aayush.koi <aayush.k204@gmail.com>
2024-07-29 22:56:39 -0400

    3nd try

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
+    apiKey: "AIzaSyCYkbi3HL62WgjL5nNjxZTXHyItw09rthE",
+    authDomain: "pantry-40e32.firebaseapp.com",
+    projectId: "pantry-40e32",
+    storageBucket: "pantry-40e32.appspot.com",
+    messagingSenderId: "914328450166",
+    appId: "1:914328450166:web:97f32819a476d08d861b57",
+    measurementId: "G-QVN07J0VFX"
   };
 
 const app = initializeApp(firebaseConfig);
